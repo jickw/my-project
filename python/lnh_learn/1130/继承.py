21d32245784c351b90f3cf35fe8c303698e8c819
wuxuan <wuxuan@my089.net>
2018-11-30 16:51:31 +0800

    添加1130

--- python/lnh_learn/1130/继承.py
+++ python/lnh_learn/1130/继承.py
@@ -1,2 +1,30 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
+
+# 面向对象三大特性： 继承，多态， 封装
+
+# 继承
+
+class Animal:
+    breath = "呼吸"
+
+    def __init__(self, name , sex, age):
+        self.name = name
+        self.sex = sex
+        self.age = age
+
+    def eat(self):
+        print('吃东西')
+
+    def drink(self):
+        print('喝东西')
+
+class Cat(Animal):
+    pass
+
+class Dog(Animal):
+    pass
+
+class Chook(Animal):  # 括号里面的  父类，基类， 超类  括号外面的 子类， 派生类
+    pass
+
