fd47ec4a301b2b0302ee9b80a74903f2d1c43e29
wuxuan <wuxuan@my089.net>
2019-01-02 11:14:16 +0800

    添加1223dd

---
+++ python/lnh_learn/201901/0102/析构方法.py
@@ -0,0 +1,21 @@
+
+# 构造方法  申请一个空间
+# 析构方法  释放一个空间
+# 不管是主动还是被动，这个f对象总会被清理掉，被清理掉就触发__del__方法，触发这个方法就会归还操作系统的文件资源
+
+class File:
+    # 处理文件的
+    def __init__(self, file_path):
+        self.f = open(file_path)
+
+    def read(self):
+        self.f.read(1024)
+
+    def __del__(self):  # 是去归还/释放一些在创建对象的时候借用的一些资源
+        # del 对象的时候
+        # python解释器的垃圾回收机制  回收这个对象所占得的内存的时候    python自动触发的
+        self.f.close()
+
+
+f = File('file.txt')
+f.read()
